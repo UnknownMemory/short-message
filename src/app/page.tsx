44d8d7787a1180e4b0ec937a34e6144b3ea8ff7d
Jérémy <jeremyschneider21@gmail.com>
2024-06-05 17:30:01 +0200

    move db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { Post } from "@/components/Post";
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center">
-      <div id="feed" className="min-h-full w-[40%] border-x">
-        <div className="text-center p-2 border-b">Following</div>
-        <div className="flex">
+      <div id="feed" className="min-h-full w-[35%]">
+        {/* <div className="text-center p-2">Following</div> */}
+        <div className="flex flex-col">
           <Post></Post>
         </div>
       </div>
