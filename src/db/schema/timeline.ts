d19c6b01c565631b36c42a33c15c5832c43d1282
UnknownMemory <jeremyschneider21@gmail.com>
2024-11-12 17:05:53 +0100

    add timeline table

---
+++ src/db/schema/timeline.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, timestamp, serial } from "drizzle-orm/pg-core";
+import { user } from "./user";
+
+
+export const timeline = pgTable('timeline', {
+    id: serial('id').primaryKey(),
+    userID: integer("user_id").references(() => user.id),
+    lastSeen: timestamp('last_seen').notNull()
+})
