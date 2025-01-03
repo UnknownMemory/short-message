20fa6ab24c25cddbf3b9f9251cfd5820a45b2061
UnknownMemory <jeremyschneider21@gmail.com>
2025-01-03 21:48:16 +0100

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,34 +15,34 @@ export const Sidebar = () => {
         staleTime: Infinity,
     })
     return (
-        <div className="md:w-44 md:mr-6 md:justify-self-end top-0 left-0 fixed bg-sm-white h-full z-10">
-            <nav className="md:text-lg flex flex-col md:mt-5 bg-sm-white h-full">
-                <Link className="md:h-11 md:px-2 md:rounded-md flex items-center text-sm-primary-dark"  href={`/${user?.username}`}>
-                    <div className="h-[44px] w-[44px] relative md:mr-2">
+        <div className="md:w-44 md:mr-6 md:justify-self-end md:static md:border-0 top-0 left-0 fixed bg-sm-white h-full z-10 border-r-[1px]">
+            <nav className="md:text-lg flex flex-col md:mt-5 bg-sm-white h-full text-xl">
+                <Link className="md:h-11 md:px-2 px-5 pt-3 md:rounded-md flex items-center text-sm-primary-dark"  href={`/${user?.username}`}>
+                    <div className="h-[44px] w-[44px] relative mr-2">
                         <Image className="rounded-full" src="/default_avatar.jpg" fill style={{objectFit: "cover"}} alt="Profile Picture" loading="lazy"></Image>
                     </div>
                     <div className="flex flex-col">
-                        <span className="text-sm">{user?.display_name}</span>
+                        <span className="md:text-sm text-lg font-bold">{user?.display_name}</span>
                         <span className="text-sm text-sm-dark-gray">@{user?.username}</span>
                     </div>
                 </Link>
-                <Link prefetch={false} className="md:h-11 md:px-2 md:mt-4 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/">
+                <Link prefetch={false} className="md:h-11 md:px-2 px-5 py-3 md:mt-4 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/">
                     <HomeIcon className="size-6 mr-1"/>
                     <span>Home</span>
                 </Link>
-                <Link prefetch={false} className="md:h-11 md:px-2 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/notifications">
+                <Link prefetch={false} className="md:h-11 md:px-2 px-5 py-3 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/notifications">
                     <BellIcon className="size-6 mr-1"/>
                     <span>Notifications</span>
                 </Link>
-                <Link prefetch={false} className="md:h-11 md:px-2 md:rounded-md flex items-center text-sm-primary-dark nav-link" href={`/${user?.username}`}>
+                <Link prefetch={false} className="md:h-11 md:px-2 px-5 py-3 md:rounded-md flex items-center text-sm-primary-dark nav-link" href={`/${user?.username}`}>
                     <UserCircleIcon className="size-6 mr-1"/>
                     <span>Profile</span>
                 </Link>
-                <Link prefetch={false} className="md:h-11 md:px-2 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/settings">
+                <Link prefetch={false} className="md:h-11 md:px-2 px-5 py-3 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/settings">
                     <Cog6ToothIcon className="size-6 mr-1"/>
                     <span>Settings</span>
                 </Link>
-                <Link prefetch={false} className="md:h-11 md:px-2 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/logout">
+                <Link prefetch={false} className="md:h-11 md:px-2 px-5 py-3 md:rounded-md flex items-center text-sm-primary-dark nav-link" href="/logout">
                     <ArrowRightEndOnRectangleIcon className="size-6 mr-1"/>
                     <span>Logout</span>
                 </Link>
