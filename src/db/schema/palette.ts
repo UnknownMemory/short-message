0425cad5311dc8dc9ecf8bcdd1c7fb66cfe8641d
UnknownMemory <jeremyschneider21@gmail.com>
2025-02-17 20:32:38 +0100

    drop non-used table

--- src/db/schema/palette.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
-
-
-export const palette = pgTable('palette', {
-    id: serial('id').primaryKey(),
-    colors: text('colors').array()
-})
