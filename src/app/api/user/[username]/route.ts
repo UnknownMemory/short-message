04cc1b7597d7403185db189ed227e3528572ccc7
UnknownMemory <jeremyschneider21@gmail.com>
2025-05-04 12:53:16 +0200

    distinct follower/following count

--- src/app/api/user/[username]/route.ts
+++ src/app/api/user/[username]/route.ts
@@ -1,16 +1,16 @@
-import { eq, and, isNotNull, count } from 'drizzle-orm';
+import { eq, and, isNotNull, countDistinct } from 'drizzle-orm';
 
 import { db } from "@/db/db"
 import { user } from "@/db/schema/user"
 import { NextResponse } from "next/server";
 import { follow } from "@/db/schema/follow";
-import { alias, PgSelect, PgTableWithColumns } from "drizzle-orm/pg-core";
+import { alias, PgSelect } from "drizzle-orm/pg-core";
 import { User } from "@/types/User";
 
 import { apiCheckAuth } from "@/utils/auth";
 
 
-const withIsFollower = <T extends PgSelect>(qb: T, userID: number) => {
+const withIsFollowing = <T extends PgSelect>(qb: T, userID: number) => {
     const isFollowing = alias(follow, "isFollowing")
     return qb.leftJoin(isFollowing, and(eq(isFollowing.userID, userID), eq(isFollowing.followingID, user.id))).groupBy(user.id, isFollowing.userID, isFollowing.followingID)
 }
@@ -37,18 +37,18 @@ export async function GET(request: Request, { params }: { params: { username: st
         image: user.image,
         description: user.description,
         created_at: user.created_at,
-        follower: count(follower),
-        following: count(following),
+        follower: countDistinct(follower),
+        following: countDistinct(following),
         ...(isUserProfile && { is_following: isNotNull(isFollowing) })
     }).from(user)
-        .leftJoin(following, eq(following.followingID, userID))
+        .leftJoin(following, eq(following.userID, user.id))
         .leftJoin(follower, eq(follower.followingID, user.id))
         .where(eq(user.username, username))
         .groupBy(user.id)
         .$dynamic()
 
     if (isUserProfile) {
-        userQuery = withIsFollower(userQuery, userID)
+        userQuery = withIsFollowing(userQuery, userID)
     }
 
     users = await userQuery
