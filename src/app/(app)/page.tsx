de9bd3ada3fe5724a9031395501abb806ff8fd48
UnknownMemory <jeremyschneider21@gmail.com>
2025-10-25 17:24:04 +0200

    chore(ui): only use dark theme

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,42 +1,51 @@
-"use client"
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
 
 import { Bars3Icon } from "@heroicons/react/24/outline";
-import { Virtuoso } from 'react-virtuoso'
+import { Virtuoso } from "react-virtuoso";
 
 import { Post } from "@/components/Post";
 import { PostInput } from "@/components/PostInput";
 import { Post as PostT } from "@/types/Post";
 
-import { useCurrentUserQuery } from '@/queries/user';
-import { useSidebarStore } from "@/stores/sidebar-store";
-import { useTimelineQuery } from '@/queries/post';
-
+import { useCurrentUserQuery } from "@/queries/user";
+import { useSidebarStore } from "@/stores/sidebar";
+import { useTimelineQuery } from "@/queries/post";
 
 export default function Home() {
-    const {setIsOpen} = useSidebarStore((state) => state)
+    const { setIsOpen } = useSidebarStore((state) => state);
 
-    const {data: me} = useCurrentUserQuery()
-    const {data: postsPages, fetchNextPage, refetch} = useTimelineQuery(!!me?.id)
+    const { data: me } = useCurrentUserQuery();
+    const { data: postsPages, fetchNextPage, refetch } = useTimelineQuery(!!me?.id);
 
-    const posts = postsPages?.pages.flatMap(page => {
-        return page.posts
-    })
+    const posts = postsPages?.pages.flatMap((page) => {
+        return page.posts;
+    });
     return (
         <>
             <div id="feed" className="min-h-full md:border-x-[1px]">
                 <nav className="grid grid-cols-3 w-full h-14 border-b-[1px]">
-                    <Bars3Icon className="size-6 self-center ml-4 md:hidden" onClick={() => setIsOpen(true)}/>
+                    <Bars3Icon className="size-6 self-center ml-4 md:hidden" onClick={() => setIsOpen(true)} />
                     <div className="flex justify-center md:col-span-3 cursor-pointer" onClick={() => refetch()}>
-                        <Image src="/logo.svg" alt="Short Message logo" width={28} height={28}/>
+                        <Image src="/logo.svg" alt="Short Message logo" width={28} height={28} />
                     </div>
                 </nav>
-                <PostInput refetch={refetch}/>
+                <PostInput refetch={refetch} />
                 <Virtuoso
                     useWindowScroll
-                    style={{height: '100%'}}
+                    style={{ height: "100%" }}
                     data={posts}
-                    itemContent={(_, post: PostT) =>{ return <Post key={post.id} post={post} isTimeline={true} currentUserId={me.id} onDelete={refetch}/>}}
+                    itemContent={(_, post: PostT) => {
+                        return (
+                            <Post
+                                key={post.id}
+                                post={post}
+                                isTimeline={true}
+                                currentUserId={me.id}
+                                onDelete={refetch}
+                            />
+                        );
+                    }}
                     endReached={(_) => fetchNextPage()}
                 />
             </div>
--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -1,23 +1,40 @@
-"use client"
-
-import { TrashIcon } from "@heroicons/react/24/outline";
+"use client";
+import { useRouter } from "next/navigation";
+import {
+    ArrowLeftIcon,
+    TrashIcon,
+    SwatchIcon,
+    SunIcon,
+    MoonIcon,
+    ComputerDesktopIcon,
+} from "@heroicons/react/24/outline";
 
 import { useCurrentUserQuery } from "@/queries/user";
 import deleteUserAction from "./actions";
 
 export default function Settings() {
-    
-    const {data: me} = useCurrentUserQuery()
+    const { data: me } = useCurrentUserQuery();
+    const router = useRouter();
 
     return (
-        <div id="settings" className="min-h-full md:border-x-[1px] px-6">
+        <div id="settings" className="min-h-full md:border-x-[1px] ">
+            <nav className="grid grid-cols-3 w-full h-14 border-b-[1px]">
+                <div className="self-center ml-4">
+                    <button type="button" onClick={router.back}>
+                        <ArrowLeftIcon className="size-5 inline"></ArrowLeftIcon>
+                    </button>
+                    <div className="md:text-lg md:col-span-1 inline md:ml-4">Settings</div>
+                </div>
+            </nav>
+
             <div className="pl-2 mt-2 flex justify-center">
                 <button className="flex items-center text-red-600" onClick={() => deleteUserAction()}>
-                    <div className="rounded-full"><TrashIcon className="size-6 m-2"></TrashIcon></div>
+                    <div className="rounded-full">
+                        <TrashIcon className="size-6 m-2"></TrashIcon>
+                    </div>
                     <span className="pl-1">Delete my account</span>
                 </button>
             </div>
-           
         </div>
     );
 }
